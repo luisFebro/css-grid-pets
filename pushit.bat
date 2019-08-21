f2ac94c9cd83a748e9516436e540a6fe3eea4af7
Luis Febro <mr.febro@gmail.com>
2019-08-20 23:31:36 -0400

    new meta elem and footer

--- pushit.bat
+++ pushit.bat
@@ -1,8 +1,8 @@
 echo "To run a batch file from terminal cd/locate_your_file and then
-<file_name>.bat or simply <file_name> and hit ENTER."
+echo <file_name>.bat or simply <file_name> and hit ENTER."
 
 
 git add .
 git commit -m %1
 echo "%1 is used to pass the dynamic commit message from the command line"
-git push
+git push --set-upstream origin master
